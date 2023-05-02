05da1643cdbcd03627d464c68e544c215373a300
sebastiannesheim <sebnes02@gmail.com>
2023-05-02 15:53:19 +0200

    Endringer

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sebastiannesheim/sem03simpleclient
+
+go 1.20
+
+require github.com/sebastiannesheim/is105sem03 v0.0.0-20230502133718-2edf80543af7
--- tcp_client.go
+++ tcp_client.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"net"
 	"log"
+	"net"
 	"os"
+
+	"github.com/sebastiannesheim/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.2:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4) //Dette kryptere meldingen
+
+	_, err = conn.Write([]byte(string(kryptertMelding))) //Dette sender den krypterte meldingen
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +26,10 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
+	response := string(buf[:n])                                                                                //Dette gjør om fra byte til string
+	log.Printf("reply from proxy: %s", response)                                                               //Printer response
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)+4) //Meldingen som komme tilbake blir dekrypt her
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))                                              //Printer den dekrypte meldingen
+
 }
